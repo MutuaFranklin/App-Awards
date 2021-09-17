050ae76dcec87445316b804582d2a82c2f4ea0fc
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-17 11:21:05 +0300

    profile views

---
+++ profiles/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Profile
+from awardsApp.models import Project
+
+# Create your views here.
+
+
+
+def userProfile(request, username):
+    otherUser = get_object_or_404(User, username=username)
+    userProfile = Profile.objects.get(user=otherUser)
+    myProfile = Profile.objects.get(user=request.user)
+    # projects = Project.objects.filter(posted_by = userProfile).order_by('-posted_on')
+
+    context ={
+        "otherUser":otherUser,
+        "userProfile":userProfile,
+        # "projects": projects,
+              
+        
+    }
+
+
+    return render(request, 'profile/userProfile.html', context)
+
+
