a9dfd3472b236d6decfad173cad10f13902fb3ad
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-17 11:19:35 +0300

    admin setup

---
+++ awardsApp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Profile, Project, Rating, Review
+
+# Register your models here.
+
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Rating)
+admin.site.register(Review)
+
+
