3c4b97d4da04c858c138b367aefff404521a9381
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-17 11:18:13 +0300

    model serializer

---
+++ awardsApp/serializer.py
@@ -0,0 +1,21 @@
+# from profiles.models import Profile
+# from rest_framework import serializers
+# from .models import Project
+
+
+
+
+
+# class ProfileSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Profile
+#         fields = ('id', 'user','link', 'profile_pic', 'location' )
+
+
+# class ProjectSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Project
+#         fields = ('id', 'title', 'link', 'description', 'technologies', 'project_image','publisher', 'date_published' )
+
+
+
