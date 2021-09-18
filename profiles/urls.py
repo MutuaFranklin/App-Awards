3fd839176f951a6c282f935ea01aa0efd0ff5de3
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-18 11:08:56 +0300

    update urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,17 +2,19 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf import settings
-
-from django.urls import re_path, path, include
+from .views  import UpdateProfileView
+from django.urls import re_path, path
 
 
 urlpatterns=[
 
     path('<username>/', views.userProfile, name='userProfile'),
+    path('update/<int:pk>', UpdateProfileView.as_view(), name='update-profile'), 
 
 
 
 ]
+
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
