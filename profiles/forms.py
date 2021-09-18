33710c2d916494e6ce53cb6699e40ba68a90db72
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-18 11:08:40 +0300

    app forms

---
+++ profiles/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.forms import widgets
+from .models import Profile
+
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [ 'profile_pic', 'bio', 'location', 'mobile', 'website', 'github']
+        widgets = {
+            'profile_pic': forms.FileInput(attrs={'class':'form-control'}),
+            'bio':forms.Textarea(attrs={'class': 'form-control'}),
+            'location':forms.TextInput(attrs={'class': 'form-control'}),
+            'mobile': forms.TextInput(attrs={'class':'form-control'}),
+            'website': forms.TextInput(attrs={'class':'form-control'}),
+            'github':forms.TextInput(attrs={'class': 'form-control'})
+        
+        }
