cb0272e077eab94447a195be36646a0e2d6259c4
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-18 16:22:03 +0300

    urls update

--- awardsApp/urls.py
+++ awardsApp/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import re_path, path, include
-
+from . import api
 
 urlpatterns=[
    
@@ -13,15 +13,13 @@ urlpatterns=[
     path('project-details/<int:id>', views.projectDetails, name='project-details'),
     path('publish-project/', views.publishProject, name='publish-project'),
     path('search/', views.search_project, name='search_project'),
+    path('api/profile/', views.ProfileList.as_view()),
+    path('api/project/', views.ProjectList.as_view()),
 
-
-
-
-
+   
     # path('login/', views.login_user, name='login'),
 
 
-   
 
 
 ]
