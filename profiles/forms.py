2bdb0afdee945c8627997e0ee0bfd925416c4cc6
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-18 16:22:35 +0300

    profile form update

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,10 +7,11 @@ from .models import Profile
 class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = [ 'profile_pic', 'bio', 'location', 'mobile', 'website', 'github']
+        fields = [ 'profile_pic', 'bio','gender','location', 'mobile', 'website', 'github']
         widgets = {
             'profile_pic': forms.FileInput(attrs={'class':'form-control'}),
             'bio':forms.Textarea(attrs={'class': 'form-control'}),
+            'gender':forms.Select(attrs={'class': 'form-control'}),
             'location':forms.TextInput(attrs={'class': 'form-control'}),
             'mobile': forms.TextInput(attrs={'class':'form-control'}),
             'website': forms.TextInput(attrs={'class':'form-control'}),
