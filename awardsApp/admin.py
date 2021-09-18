d352bbad62f6036d612d57d42836114aaa10d479
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-18 23:10:19 +0300

    admin update

--- awardsApp/admin.py
+++ awardsApp/admin.py
@@ -3,8 +3,11 @@ from .models import Profile, Project, Rating, Review
 
 # Register your models here.
 
+class UserFollow(admin.ModelAdmin):
+    filter_horizontal =('following','followers')
 
-admin.site.register(Profile)
+
+admin.site.register(Profile, UserFollow)
 admin.site.register(Project)
 admin.site.register(Rating)
 admin.site.register(Review)
