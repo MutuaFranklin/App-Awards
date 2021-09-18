c68c15cf429b6999f259a0fb884b985ddd1c2d92
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-18 11:09:08 +0300

    update views

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from .models import Profile
 from awardsApp.models import Project
+from .forms import UpdateUserProfileForm
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
+
+
+
 
 # Create your views here.
 
@@ -22,5 +29,22 @@ def userProfile(request, username):
 
 
     return render(request, 'profile/userProfile.html', context)
+
+
+
+
+class UpdateProfileView(UpdateView):
+        model=Profile
+        slug_field = "username"
+        form_class =UpdateUserProfileForm
+        template_name ='profile/editProfile.html'
+        
+        def get_queryset(self): 
+            return Profile.objects.all()
+
+
+        def get_success_url(self):
+        
+            return reverse_lazy('userProfile',args=[self.request.user.username]) 
 
 
